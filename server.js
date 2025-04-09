3a89c56729a674454caa191bde83e9435b7c76c5
nalcakar <nalcakar@gmail.com>
2025-04-09 23:35:35 +0300

    server.js dosyası taşındı ve bağımlılıklar güncellendi

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Credentials", "true"); // ✅ EKLENDİ
   }
-  // Diğer gerekli CORS başlıkları
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
